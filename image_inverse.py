9f6fcadf53a70584b2c778eeee10bc869c24623f
Doris-UESTC <yirandoris@gmail.com>
2024-01-01 13:53:24 +0800

    fix normalize bug

--- image_inverse.py
+++ image_inverse.py
@@ -77,8 +77,8 @@ def main():
 
     cfg = load_yaml(args.cfg)
     zeta = cfg['zeta']
-    # transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
+    transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
     dataset = get_dataset(**cfg['data'], transforms=transform)
     loader = get_dataloader(dataset, batch_size=1, num_workers=0, train=False)
     operator = get_operator(device=device, **cfg['operator'])
